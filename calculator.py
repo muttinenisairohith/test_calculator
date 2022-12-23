69af399e033448de7f25d56e14937a306d34e48d
muttinenisairohith <muttinenisairohith@gmail.com>
2022-12-23 18:55:32 +0530

    adding multiplication code

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
 def add(a, b):
-	return a+b+5
+	return a+b
 
 def sub(a, b):
 	return a-b
+
+def mul(a, b):
+	return a * b
 
 a = int(input())
 b = int(input())
 print(add(a, b))
 print(sub(a, b))
+print(mul(a, b))
